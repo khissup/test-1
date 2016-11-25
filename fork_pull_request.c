145d7bceb0f6ab0a32ac1ba3595378ced20f7d72
khissup <khissup@naver.com>
2016-11-25 15:15:25 +0900

    Update fork_pull_request.c

--- fork_pull_request.c
+++ fork_pull_request.c
@@ -3,10 +3,13 @@
 int main()
 {
   int i;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+  for (i = 0; i < 10; i++)
+  {
+    for (j = 0; j < 10; j++)
+    {
+      printf("%d * %d = %d \n", i, j, i * j);
     }
+    printf("-------------------\n");
   }
   
   return 0;
